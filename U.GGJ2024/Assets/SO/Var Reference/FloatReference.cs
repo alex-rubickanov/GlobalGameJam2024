6d3aae5f557a9b7e82568aa62a8db9e91c9e280d
Vince Delgado <116175769+VKD07@users.noreply.github.com>
2024-01-28 03:35:04 +0400

    Added FInal Lighting to the scene

---
+++ U.GGJ2024/Assets/SO/Var Reference/FloatReference.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FloatReference", menuName = "SO/FloatReference")]
+public class FloatReference : ScriptableObject
+{
+    public float value;
+}
